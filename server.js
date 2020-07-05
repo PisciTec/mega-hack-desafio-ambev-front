28ac96832c10c59efec4a665411452812708d614
Raian <raiannicacio@id.uff.br>
2020-07-05 14:58:47 -0300

    Atualização no Server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(express.static("MegaHack"));
 // default URL for website
@@ -10,6 +13,5 @@ app.use('/', function(req,res){
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
+server.listen(PORT);
 console.debug('Server listening on port ' + port);
